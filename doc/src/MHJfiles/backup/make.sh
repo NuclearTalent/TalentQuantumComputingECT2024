d193070ab7115c81fc019e773c89aa7fa53c0fe8
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2025-06-16 13:59:34 +0200

    added files

---
+++ doc/src/MHJfiles/backup/make.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+set -x
+
+function system {
+  "$@"
+  if [ $? -ne 0 ]; then
+    echo "make.sh: unsuccessful command $@"
+    echo "abort!"
+    exit 1
+  fi
+}
+
+if [ $# -eq 0 ]; then
+echo 'bash make.sh slides1|slides2'
+exit 1
+fi
+
+name=$1
+rm -f *.tar.gz
+
+opt="--encoding=utf-8"
+# Note: Makefile examples contain constructions like ${PROG} which
+# looks like Mako constructions, but they are not. Use --no_mako
+# to turn off Mako processing.
+opt="--no_mako"
+
+rm -f *.aux
+
+
+
+
+# IPython notebook
+system doconce format ipynb $name $opt
+
+
+# LaTeX Beamer slides
+beamertheme=red_plain
+system doconce format pdflatex $name --latex_title_layout=beamer --latex_table_format=footnotesize $opt
+system doconce ptex2tex $name envir=minted
+# Add special packages
+doconce subst "% Add user's preamble" "\g<1>\n\\usepackage{simplewick}" $name.tex
+system doconce slides_beamer $name --beamer_slide_theme=$beamertheme
+system pdflatex -shell-escape ${name}
+system pdflatex -shell-escape ${name}
+cp $name.pdf ${name}.pdf
+cp $name.tex ${name}.tex
+
+
+
+
+
+
+
+
